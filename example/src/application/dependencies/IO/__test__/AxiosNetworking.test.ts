2aa035fc8c7350c64c0b4f628b4cc17933c49c3b
김규화 <kgh@itsdcode.com>
2022-03-24 12:37:42 +0900

    (fix): NCNetwork : baseUrl 관련 테스트 코드 - NCNetwork()를 사용하지 않고, AxiosNetworking()을 직접 사용하도록 테스트코드를 수정

--- example/src/application/dependencies/IO/__test__/AxiosNetworking.test.ts
+++ example/src/application/dependencies/IO/__test__/AxiosNetworking.test.ts
@@ -1,43 +1,11 @@
 import { AxiosNetworking } from "../AxiosNetworking";
 import axios, { AxiosRequestConfig } from "axios";
-import {
-  NCApplicationConfiguration,
-  NCApplicationContext,
-  NCLog,
-  NCNetwork,
-  NCNetworkingClassName,
-} from "@ncodedcode/ncode_react_lib";
-import { NCLogLevel } from "@ncodedcode/ncode_react_lib/dist/utils/NCLog";
-import { ServiceLocator } from "../../../../../../src";
-
-class MockAppConfiguration implements NCApplicationConfiguration {
-  application(app: NCApplicationContext): void {
-    app.devMode = process.env.REACT_APP_ENV_TYPE === "dev";
-    NCLog.setLogLevel(NCLogLevel.Debug);
-  }
-
-  config(locator: ServiceLocator): void {
-    locator.registFactory(
-      NCNetworkingClassName,
-      () => this._dependencies.NCNetworking
-    );
-  }
-
-  private _dependencies: { [key: string]: any } = {};
-
-  constructor(dependencies: { [key: string]: any }) {
-    this._dependencies = dependencies;
-  }
-}
+import { NCNetworkMethod } from "@ncodedcode/ncode_react_lib";
 
 describe("AxiosNetworking test", () => {
   describe("baseUrl test", () => {
-    const baseUrl = `${process.env.REACT_APP_TEST_BASE_URL}`;
-    const path = "/users/1";
-
-    NCApplicationContext.createContext(
-      new MockAppConfiguration({ NCNetworking: new AxiosNetworking(baseUrl) })
-    );
+    const baseUrl = "https://jsonplaceholder.typicode.com";
+    const axiosNetworking = new AxiosNetworking(baseUrl);
 
     let reqConfig: AxiosRequestConfig;
     axios.interceptors.request.use((config) => {
@@ -46,13 +14,27 @@ describe("AxiosNetworking test", () => {
     });
 
     test('request to "path"', async () => {
-      await new NCNetwork().get(path);
+      const path = "/users/1";
+      await axiosNetworking.execute(
+        NCNetworkMethod.GET,
+        path,
+        undefined,
+        undefined,
+        {},
+        true
+      );
       expect(reqConfig.url).toBe(`${baseUrl}${path}`);
     });
-
     test('request to "full url"', async () => {
-      const anotherUrl = `${process.env.REACT_APP_WORLD_TIME_API_URL}`;
-      await new NCNetwork().get(anotherUrl);
+      const anotherUrl = "http://worldtimeapi.org/api";
+      await axiosNetworking.execute(
+        NCNetworkMethod.GET,
+        anotherUrl,
+        undefined,
+        undefined,
+        {},
+        true
+      );
       expect(reqConfig.url).toBe(anotherUrl);
     });
   });
